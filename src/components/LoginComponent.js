3ab43f91660881b7f8e64b01316880b5a4d05161
Chris Anderson <cl.anderson8@gmail.com>
2018-07-18 11:07:15 -0500

    login component boilerplate

---
+++ src/components/LoginComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import firebase from 'firebase';
+
+class LoginComponent extends React.Component {
+  constructor(props){
+    super()
+    this.state = {};
+  }
+
+  render() {
+    return <div></div>
+  }
+}
+
+export default LoginComponent;
