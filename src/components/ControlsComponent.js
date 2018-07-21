7485dac1eeec4f31e6a0e39db3a5eabe9adcee79
Chris Anderson <cl.anderson8@gmail.com>
2018-07-20 21:59:43 -0500

    Controls Component boilerplate

---
+++ src/components/ControlsComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import firebase from 'firebase';
+
+class ControlsComponent extends React.Component {
+  render(){
+    return <div></div>
+  }
+}
+
+export default ControlsComponent;
